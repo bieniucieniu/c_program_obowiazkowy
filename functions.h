ce18303560df97ca743ef91f794c218cd284853b
Bieniu <mikibien@outlook.com>
2022-03-28 22:07:27 +0200

    update

--- functions.h
+++ functions.h
@@ -1,77 +1,59 @@
-#define NULL 0
 
 #ifndef __FUNCTIONS__
 #define __FUNCTIONS__
+#define NULL 0
 
 
 
 void CopeString(char pcSource[], char pcDestination[]) {
-    for (short i = 0; pcSource[i] != NULL; i++) {
-        pcDestination[i] = pcSource[i];
+
+    unsigned char ucCharCounter;
+    for (ucCharCounter = 0; pcSource[ucCharCounter]; ucCharCounter++) {
+        pcDestination[ucCharCounter] = pcSource[ucCharCounter];
     }
-}
-
-
-void CopeStringWhile(char pcStr1[], char pcStr2[]) {
-
-    short i = 0;
-    do {
-        pcStr2[i] = pcStr1[i++];
-    } while (pcStr1[i]!=NULL);
-
-    pcStr2[i] = NULL;
+    pcDestination[ucCharCounter] = pcSource[ucCharCounter];
 }
 
 
 enum EEquivalent{NOTEQUEL, EQUEL};
 
 enum EEquivalent eCompareStr(char str1[], char str2[]) {
-    for (short i = 0; str1[i] != NULL || str2[i] != NULL; i++) {
-        if(str1[i] != str2[i]) return NOTEQUEL;
+    for (unsigned char ucCharCounter = 0; str1[ucCharCounter] || str2[ucCharCounter]; ucCharCounter++) {
+        if(str1[ucCharCounter] != str2[ucCharCounter]) return NOTEQUEL;
     }
     return EQUEL;
 }
 
 
 void AppendString(char pcSourceStr[], char pcDestinationStr[]) {
-    for (short i = 0; pcSourceStr[i] != NULL ; i++) {
-        if(pcDestinationStr[i] == NULL){
-            pcDestinationStr[i] = pcSourceStr[i];
-        }
-    }
+    unsigned char ucCharCounter = 0;
+    for (ucCharCounter = 0; pcDestinationStr[ucCharCounter] != NULL ; ucCharCounter++);
+    CopeString(pcSourceStr, pcDestinationStr + ucCharCounter);
 }
 
 
 void ReplaceCharactersInString(char str[],char cOldChar,char cNewChar) {
-    for(short i = 0; str[i]!=NULL; i++){
+    for(short i = 0; str[i]; i++){
         if(str[i] == cOldChar) str[i] = cNewChar;
     }
 }
 
-void IntToHexStr(unsigned int uiValue, char pcStr[]) {
-    char hexs_numbers[17] = "0123456789ABCDEF";
+void UIntToHexStr(unsigned int uiValue, char pcStr[]) {
+    pcStr[0] = '0';
+    pcStr[1] = 'x';
+    pcStr[6] = NULL;
 
-    for (short i = 2; uiValue != NULL; i++){
-        pcStr[i] = hexs_numbers[uiValue & 0b1111];
-        uiValue = uiValue >> 4;
+    unsigned char ucBuffer;
+
+    for(char i = 0; i < 4; i++) {
+        ucBuffer = uiValue & 0b1111;
+        pcStr[5 - i] = ucBuffer < 10 ? '0' + ucBuffer : 'A' + ucBuffer - 10;
     }
-    pcStr = pcStr+NULL;
 }
 
 
-void HexStringToUInt(char pcStr[], unsigned int *puiValue) {
-    char __hexs_numbers[17] = "0123456789ABCDEF";
-
-    puiValue = 0;
-    for (short i = 2; pcStr[i] != NULL; i++) {
-        for (short j = 0; __hexs_numbers[j] != NULL; j++) {
-            if(pcStr[i] == __hexs_numbers[j]) {
-                *puiValue = *puiValue << 4;
-                *puiValue = (*puiValue | j);
-                break;
-            }
-        }
-    }
+void eHexStringToUInt(char pcStr[], unsigned int *puiValue) {
+    
 }
 
 #endif
